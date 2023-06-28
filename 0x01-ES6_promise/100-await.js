9034a3e8266c498a27551735f496c9777a02457f
Emenike Nathaniel <emenikenathaniel55@gmail.com>
2023-06-28 00:34:27 +0000

    Es6 promise

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,20 +1,12 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-async function asyncUploadUser() {
+export default async function asyncUploadUser() {
   try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    
-    return {
-      photo,
-      user,
-    };
+    const prom1 = await uploadPhoto();
+    const prom2 = await createUser();
+
+    return ({ photo: prom1, user: prom2 });
   } catch (error) {
-    console.error(error); // Optional: log the error for debugging purposes
-    return {
-      photo: null,
-      user: null,
-    };
+    return ({ photo: null, user: null });
   }
 }
-
